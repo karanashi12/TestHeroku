6aa1fec45ed8d502a132bb2c795539d2ce8853ac
karanashi12 <karanashiuchiha@gmail.com>
2019-09-01 22:13:30 +0700

    1

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
     }
 </style>
 <div class="login-form">
-    <form action="/examples/actions/confirmation.php" method="post">
+    <form method="post">
         <h2 class="text-center">Log in</h2>       
         <div class="form-group">
             <input type="text" class="form-control" placeholder="Username" required="required" name="username">
