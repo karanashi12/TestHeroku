fd42c7edd82278a836dcbc01ef3f58bb3ca76dc2
karanashi12 <karanashiuchiha@gmail.com>
2019-09-02 11:32:10 +0700

    1

--- index.php
+++ index.php
@@ -37,13 +37,13 @@
     <form method="post" action="Admin.php">
         <h2 class="text-center">Log in</h2>       
         <div class="form-group">
-            <input type="text" class="form-control" placeholder="Username" required="required" id="username">
+            <input type="text" class="form-control" placeholder="Username" required="required" name="username">
         </div>
         <div class="form-group">
-            <input type="password" class="form-control" placeholder="Password" required="required" id="password">
+            <input type="password" class="form-control" placeholder="Password" required="required" name="password">
         </div>
         <div class="form-group">
-            <button type="submit" class="btn btn-primary btn-block" id="login">Log in</button>
+            <button type="submit" class="btn btn-primary btn-block" name="login">Log in</button>
         </div>   
     </form>
 </div>
